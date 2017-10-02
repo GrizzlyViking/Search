fcd047f283421807246ab5995383c651d195b499
Sebastian Scheel Edelmann <seb@wordery.com>
2017-10-02 17:21:35 +0100

    basic setup plumbed

---
+++ tests/Unit/BuildBasicQueryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit;
+
+use BoneCrusher\Api\Search\Book;
+use BoneCrusher\Http\Requests\SearchTerms;
+use GrizzlyViking\QueryBuilder\QueryBuilder;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BuildBasicQueryTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $parameters = $this->app->make(SearchTerms::class);
+
+        $parameters->replace($post = [
+            'term'       => 'fire publisher:someone author:"J K Rawlings"',
+            'formats'    => 'paperback',
+            'recent'     => true,
+            'categories' => 'Y',
+            'match'      => 'author'
+        ]);
+
+        $parameters->validate();
+
+        $search = new Book(new QueryBuilder(), $parameters);
+
+        dd($search);
+
+        $this->assertTrue(true);
+    }
+}
