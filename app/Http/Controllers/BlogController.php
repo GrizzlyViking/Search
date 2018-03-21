bfce6dd5ff6a8017b52df8733c278e0d54082d1f
seb <seb@wordery.com>
2018-03-21 17:33:03 +0000

    readme.md updated with the routes

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function index()
+    {
+        // TODO: create the retrieval of blog-categories
+        return $this;
+    }
+}
